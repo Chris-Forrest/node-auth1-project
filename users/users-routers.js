9af84753aedeb0d4a08c0237610e9cfcbab7313a
Chris Forrest <redrider_cr@yahoo.com>
2020-09-02 21:27:27 -0400

    login working

--- users/users-routers.js
+++ users/users-routers.js
@@ -19,7 +19,28 @@ router.post("/api/register", async (req,res,next) =>{
     }catch(err){
         next(err)
     }
-})
+});
+
+router.post("/api/login", async (req,res,next) =>{
+    try{
+        const { username, password } = req.body
+        const user = await Users.findBy({ username }).first()
+
+        if(!user) {
+            return res.status(401).json({ message: "You shall not pass"})
+        }
+
+        const passwordValid = await bcrypt.compare(password, user.password)
+        if(!password) {
+            return res.status(401).json({ message: "You shall not pass"})
+        }
+
+        req.session.user = user
+        res.json({ message: `Welcome ${user.username} !`})
+    }catch(err){
+        next(err)
+    }
+});
 
 
 module.exports = router
